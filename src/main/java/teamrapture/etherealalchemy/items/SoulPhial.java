fb58d280283fbdeae81d9f7dfc8eed37be5badd0
Unknown <roxxanrebirth@gmail.com>
2018-07-08 20:03:27 -0400

    Test

--- src/main/java/teamrapture/etherealalchemy/items/SoulPhial.java
+++ src/main/java/teamrapture/etherealalchemy/items/SoulPhial.java
@@ -1,4 +1,20 @@
 package teamrapture.etherealalchemy.items;
 
-public class SoulPhial {
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
+
+public class SoulPhial extends ItemBase {
+    public SoulPhial() {
+        super("soul_phial");
+    }
+
+    @Override
+    public String getGuideInformation() {
+        return I18n.format("soul_phial.entry");
+    }
+
+    @Override
+    public ResourceLocation getEntryIcon() {
+        return new ResourceLocation("");
+    }
 }
