1ce80549b83e89c3bf0a270856bc74fd95ec2086
Unknown <roxxanrebirth@gmail.com>
2018-07-11 03:24:10 -0400

    Benign Soul Extraction Event
    -added sound to event and status chat message indication type of soul extracted!

--- src/main/java/teamrapture/etherealalchemy/registry/events/EtherealEvents.java
+++ src/main/java/teamrapture/etherealalchemy/registry/events/EtherealEvents.java
@@ -1,9 +1,18 @@
 package teamrapture.etherealalchemy.registry.events;
 
+import net.minecraft.client.audio.Sound;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,12 +23,14 @@ import teamrapture.etherealalchemy.registry.ModItems;
 public class EtherealEvents {
 
     @SubscribeEvent
-    public static void onDeathEvent(LivingDeathEvent event) {
+    public static void benignSoulExtraction(LivingDeathEvent event) {
         if (event.getEntityLiving() instanceof EntityAnimal) {
             if(event.getSource().getTrueSource() instanceof EntityPlayer){
                 EntityPlayer player = (EntityPlayer) event.getSource().getTrueSource();
                 if(!player.getHeldItemOffhand().isEmpty()){
                     if(ItemHandlerHelper.canItemStacksStack(player.getHeldItemOffhand(), new ItemStack(ModItems.soulPhial))){
+                        player.sendStatusMessage(new TextComponentString(TextFormatting.BOLD.DARK_PURPLE + I18n.format("benign_soul_extracted.msg")),true);
+                        player.world.playSound(null,player.getPosition(),SoundEvents.ENTITY_GHAST_SCREAM,SoundCategory.PLAYERS,1.0F,-2.0F);
                         player.setHeldItem(EnumHand.OFF_HAND, new ItemStack(ModItems.filledSoulPhial));
                     }
                 }
