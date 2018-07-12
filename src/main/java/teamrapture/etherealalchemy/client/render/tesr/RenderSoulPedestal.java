2a5b833a9c474be135b2889016acdc62389bc319
abused_master <techperson71@gmail.com>
2018-07-12 00:22:36 -0400

    Added shader, now time for rendering cool stuffz

--- src/main/java/teamrapture/etherealalchemy/client/render/tesr/RenderSoulPedestal.java
+++ src/main/java/teamrapture/etherealalchemy/client/render/tesr/RenderSoulPedestal.java
@@ -1,12 +1,20 @@
 package teamrapture.etherealalchemy.client.render.tesr;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import org.lwjgl.opengl.GL11;
 import teamrapture.etherealalchemy.tiles.TileSoulPedestal;
+import teamrapture.etherealalchemy.utils.EnumPedestalType;
+import teamrapture.etherealalchemy.utils.shader.ShaderUtils;
 
 public class RenderSoulPedestal extends TileEntitySpecialRenderer<TileSoulPedestal> {
 
@@ -24,9 +32,30 @@ public class RenderSoulPedestal extends TileEntitySpecialRenderer<TileSoulPedest
                 }
 
                 if(te.isWorking) {
-                    GlStateManager.rotate((System.currentTimeMillis() / 10) % 360, 0, 1, 0);
+                    GlStateManager.rotate(System.currentTimeMillis() * 4 % 360, 0, 1, 0);
+                    switch (te.checkShape()) {
+                        case CIRCLE:
+                            break;
+                        case DIAMOND:
+                            break;
+                        case PLUS:
+                            GlStateManager.pushMatrix();
+                            GlStateManager.enableBlend();
+                            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+                            GlStateManager.disableAlpha();
+                            GlStateManager.color(1F, 1F, 1F, 0.9f);
+                            GlStateManager.translate(x, y + 5, z);
+                            GlStateManager.rotate(90F, 1F, 0F, 0F);
+                            GlStateManager.scale(5, 5, 5);
+                            //ShaderUtils.useShader(ShaderUtils.soulPedestal);
+
+                            GlStateManager.enableAlpha();
+                            GlStateManager.disableBlend();
+                            GlStateManager.popMatrix();
+                            break;
+                    }
                 }else {
-                    GlStateManager.rotate((System.currentTimeMillis() / 5) % 360, 0, 1, 0);
+                    GlStateManager.rotate((System.currentTimeMillis() / 10) % 360, 0, 1, 0);
                 }
                 RenderHelper.disableStandardItemLighting();
                 GlStateManager.alphaFunc(516, 0.003921569F);
